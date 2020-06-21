342f2da421043d10593a3805124ad2fc06430358
FlavioBS <bzsflavio@gmail.com>
2020-06-21 01:13:59 -0300

    test de formulario

--- __tests__/components/TechList.test.js
+++ __tests__/components/TechList.test.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import {render, fireEvent} from '@testing-library/react'
 
 import TechList from '../../src/components/TechList'
@@ -6,10 +7,28 @@ describe('TechList component', () => {
   it('should be able to add new tech', () => {
     //criar um arquivo e renderizar um HTML/DOM fake para realizar as verificações
     //retorna metodos para selecionar elementos dentro do component indicado
-    const { getByText } = render(<TechList/>);
+    
+    // <button>Adicionar</button> ==> getByText('Adicionar') selecionar elemento que tenha o texto indicado
+    
+    // <button data-testid="xxxx"></button> ==> getByTextId('xxxx') selecionar elemento que tenha a propriedade data-testid indicada
+    
+    // getByLabelText - selecionar o input relacionado a propriedade htmlFor da label 
+      // <label htmlFor="tech">Tech</label>
+      // <input id='tech'>
+    const { getByText, getByTestId, debug, getByLabelText } = render(<TechList/>);
 
-    fireEvent.click(getByText('Adicionar'));
+    //console.log da estrutura do html gerada para visualizar como está sendo renderizado com as ações no component
+    // debug()
+    // simular ações/eventos que um usuario irá realizar
+    // fireEvent.click(getByText('Adicionar'));
+    // debug()
+    // expect(getByText('Node.js')).toBeTruthy()
+    // expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'))
 
-    expect(getByText('Node.js')).toBe()
+    fireEvent.change(getByLabelText('Text'), { target: { value: 'Node.js'}})
+    fireEvent.submit(getByTestId('tech-form'))
+    expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'))
+    expect(getByLabelText('Tech')).toHaveValue('')
+    
   })
 })
--- src/components/TechList/index.js
+++ src/components/TechList/index.js
@@ -1,1 +1,23 @@
-import react from 'react'
+import React, {useState} from 'react'
+
+export default function TechList(){
+  const [ techs, setTechs] = useState([]);
+  const [ newTech, setNewTech] = useState('');
+
+  function handleAddTech(){
+    setTechs([...techs, newTech])
+    setNewTech('')
+  }
+
+  return(
+    <form data-testid="tech-form" onSubmit={handleAddTech}> 
+      <ul data-testid="tech-list">
+        {techs.map(tech => <li key={tech}>{tech}</li>)}
+      </ul>
+
+      <label htmlFor="tech">Tech</label>
+      <input id="tech" value={newTech} onChange={e => setNewTech(e.target.value)}></input>
+      <button onClick={handleAddTech}>Adicionar</button>
+    </form>
+  )
+}
