02bad80972b92611b2d38f5370b63fef882fe3ea
FlavioBS <bzsflavio@gmail.com>
2020-06-22 00:11:39 -0300

    mock do axios

--- __tests__/store/sagas/techs.test.js
+++ __tests__/store/sagas/techs.test.js
@@ -1,13 +1,20 @@
 //resposnsavel por executar um saga
 import {runSaga} from 'redux-saga'
+import MockAdapter from 'axios-mock-adapter'
+import api from '../../../src/services/api'
 
-import {getTechsSuccess} from '../../../src/store/modules/techs/actions'
+import {getTechsSuccess, getTechsFailure} from '../../../src/store/modules/techs/actions'
 import {getTechs} from '../../../src/store/modules/techs/sagas'
+
+const apiMock = new MockAdapter(api)
 
 describe('Techs sagas', ()=> {
   it('should be able to fetch techs', async () => {
     
     const dispatch = jest.fn();
+    
+    //interceptar as chamadas a api no saga
+    apiMock.onGet('techs').reply(200, ['Node.js'])
 
     // monitorar o saga e toda vez que for chamado uma função, será sobrescrita pela dispacth do jest 
     //simular o efeito de put do saga
@@ -15,4 +22,16 @@ describe('Techs sagas', ()=> {
        
     expect(dispatch).toHaveBeenCalledWith(getTechsSuccess(['Node.js']))
   })
+
+  it('should fail when api returns error', async () => {
+    
+    const dispatch = jest.fn();
+    
+    apiMock.onGet('techs').reply(500)
+
+    await runSaga({dispatch}, getTechs).toPromise();
+       
+    expect(dispatch).toHaveBeenCalledWith(getTechsFailure())
+  })
+  
 })
