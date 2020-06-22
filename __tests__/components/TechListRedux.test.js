4d4ff38ffc9f6665f73a7027b1995b5426a64808
FlavioBS <bzsflavio@gmail.com>
2020-06-21 21:04:24 -0300

    mock useDispatch

--- __tests__/components/TechListRedux.test.js
+++ __tests__/components/TechListRedux.test.js
@@ -2,6 +2,8 @@ import React from 'react'
 import {render, fireEvent} from '@testing-library/react'
 import { useSelector, useDispatch } from 'react-redux'
 
+import {addTech} from '../../src/store/modules/techs/actions'
+
 import TechListRedux from '../../src/components/TechListRedux'
 
 //funções ficticias irao sobrescrever as funções do componente
@@ -18,8 +20,27 @@ describe('TechList component', () => {
     const {getByTestId, getByText, debug} = render(<TechListRedux/>)
 
     debug()
-    
+
     expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'))
     expect(getByTestId('tech-list')).toContainElement(getByText('ReactJS'))
+  })
+
+  it('should be able to add new tech', ()=>{
+    const { getByTestId, getByLabelText } = render(<TechListRedux/>);
+    
+    //mock da função 
+    const dispatch = jest.fn();
+
+    //quando o useDispatch for chamado no componente, será retornado o mock do jest (criado acima)
+    useDispatch.mockReturnValue(dispatch)
+
+    fireEvent.change(getByLabelText('Text'), { target: { value: 'Node.js'}})
+    fireEvent.submit(getByTestId('tech-form'))
+
+    //imprimir todas as chamadas que a funçao dispach aconteceu
+    console.log(dispatch.mock.calls)
+
+    // expect(dispatch).toHaveBeenCalled()
+    expect(dispatch).toHaveBeenCalledWith(addTech('Node.js'))
   })
 })
